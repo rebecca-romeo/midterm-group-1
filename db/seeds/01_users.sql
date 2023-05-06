1aea805edd8ec2dc6a3437c6eb531fb6f483f0c2
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-06 19:22:04 -0400

    insert seed data to tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
--- Users table seeds here (Example)
--- INSERT INTO users (name) VALUES ('Alice');
--- INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Isabella', 'Stone', 'isabella.stone99@gmail.com', 'pw0');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Elijah', 'Rodriguez', 'elrodriguez@gmail.com', 'pw0');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Sophia', 'Kim', 'sophia_kim@outlook.ca', 'pw0');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Oliver', 'Baker', 'oliver_baker@gmail.com', 'pw0');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Chloe', 'Nguyen', 'chloenguyen07@hotmail.com', 'pw0');
