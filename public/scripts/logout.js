f692b4a8a7793d923063761f26fde23d5c589469
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-08 13:20:02 -0400

    login/logout scripts, commented out, will check if these needed. saving for now.

---
+++ public/scripts/logout.js
@@ -0,0 +1,12 @@
+// $(() => {
+//   $('#logout').on('click', (event) => {
+//     console.log("hello")
+//     $.ajax({
+//       url: '/auth/logout',
+//       method: 'GET',
+//       success: () => {
+//         window.location.href = '/auth/login';
+//       }
+//     });
+//   });
+// });
