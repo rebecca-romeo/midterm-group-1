070cfe66537c75a191514d046645320c91741a06
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-08 18:06:01 -0400

    update to check if user is logged in

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router  = express.Router();
+const getFavs = require('../db/queries/favorites');
 
 router.get('/', (req, res) => {
+
+  // Check if the user is logged in
+  if (!req.session.user) {
+    res.send("Please login to view your favorites")
+  }
+
+
   const templateVars = { user: req.session.user }
   res.render('favorites', templateVars)
 })
