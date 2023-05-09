dac7d8dd10a959d2835088a4793434f3c5674189
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-08 21:12:36 -0400

    add getFavs fn to retrieve db favorites data and pass it to the ejs

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const router  = express.Router();
-const getFavs = require('../db/queries/favorites');
+const router = express.Router();
+const { getFavs } = require('../db/queries/favorites');
 
 router.get('/', (req, res) => {
-
   // Check if the user is logged in
   if (!req.session.user) {
-    res.send("Please login to view your favorites")
+    res.send("Please login to view your favorites");
+    return;
   }
 
-
-  const templateVars = { user: req.session.user }
-  res.render('favorites', templateVars)
-})
-
+  getFavs()
+    .then((favorites) => {
+      const templateVars = { user: req.session.user, favorites };
+      res.render('favorites', templateVars);
+    })
+    .catch(err => res.json(err));
+});
 
 module.exports = router;
+// const favorites = res.json(result);
+// const templateVars = { user: req.session.user, favorites }
+// return res.render('favorites', templateVars)
