fc122b28a3e0099bf700754eb2db0579cf5eeeb6
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-09 16:31:09 -0400

    passed in data as a parameterin res.render

--- routes/item.js
+++ routes/item.js
@@ -5,9 +5,11 @@ const { getItemById } = require('../db/queries/getItemById');
 router.get('/:id', (req, res) => {
   const id = req.params.id;
   getItemById(id)
+
   .then((item) => {
     console.log(item);
-    res.render('item');
+    const templateVars = {item:item}
+    res.render('item', templateVars);
   })
   .catch((err) => {
     next(err);
