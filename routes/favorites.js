86594547ce8efc61b3601540a7f265d0aba06d3b
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-08 22:49:48 -0400

    update route & css to display page when user tries to view /favorites but not signed in

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const { getFavs } = require('../db/queries/favorites');
 
 router.get('/', (req, res) => {
+  const user = req.session.user;
   // Check if the user is logged in
   if (!req.session.user) {
-    res.send("Please login to view your favorites");
-    return;
+    return res.render('favs-signed-out-err', {user});
+
   }
 
   // DB is queried by calling the getFavs fn, returns data related to favorite items
   // pass this data to templateVars to use in ejs
   getFavs()
     .then((favorites) => {
-      const templateVars = { user: req.session.user, favorites };
+      const templateVars = { user, favorites };
       res.render('favorites', templateVars);
     })
     .catch(err => res.json(err));
