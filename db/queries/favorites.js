d3b9bc98986f7e40a00f04b28d0e6257d7febac9
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-09 23:19:35 -0400

    remove dead code

--- db/queries/favorites.js
+++ db/queries/favorites.js
@@ -21,25 +21,7 @@ const removeFav = (user, item_id) => {
 `, [user, item_id])
   }
 
-
 module.exports = { getFavs, removeFav };
 
-
-// const db = require('../connection');
-
-// const getFavs = () => {
-//   return db.query(`
-//     SELECT items.photo, items.price, items.title, items.location_city, items.location_province
-//     FROM favorites
-//     JOIN items ON favorites.item_id = items.id
-//     JOIN users ON favorites.user_id = users.id
-//     WHERE user_id = 3;
-//   `)
-//     .then(data => {
-//       return data.rows;
-//     });
-// };
-
-// module.exports = { getFavs };
 
 
