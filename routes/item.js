56e55b43ec21cd30b893c1701fe3b26bf0c9077a
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-09 20:05:37 -0400

    added route to redirect to item page

--- routes/item.js
+++ routes/item.js
@@ -5,7 +5,6 @@ const { getItemById } = require('../db/queries/getItemById');
 router.get('/:id', (req, res) => {
   const id = req.params.id;
   getItemById(id)
-
   .then((item) => {
     console.log(item);
     const templateVars = {item:item}
