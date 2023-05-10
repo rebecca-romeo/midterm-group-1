9852b687c4ee1ed6363b044271e5e0d893cef275
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-10 09:29:18 -0400

    update delete functionality with ajax

--- routes/favorites.js
+++ routes/favorites.js
@@ -27,22 +27,17 @@ router.post("/:id/delete", (req, res) => {
   const user = req.session.user;
   const item_id = req.params.id;
 
-
+// If user is not signed in, direct them to err page
   if (!user) {
     return res.render('favs-signed-out-err', {user});
   }
 
-  // if (urlDatabase[id].userID !== user.id) {
-  //   return res.status(403).send("You do not have permission to delete a URL that is not yours.");
-  // }
-
-  // if (!urlDatabase[req.params.id]) {
-  //   return res.status(404).send("Invalid tinyURL, please try again.");
-  // }
-
   removeFav(user, item_id)
   .then(() => {
+
+    // res.status(200).send("OK");
     res.redirect('/favorites');
+
   })
   .catch(err => res.json(err));
 
