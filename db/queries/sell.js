341b4234cc79c4227ecaf704a4654e2d6ab50695
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-10 20:20:00 -0400

    add getUserIdFromEmail fn, update createSellListing fn to get the user id and photo and insert into db

--- db/queries/sell.js
+++ db/queries/sell.js
@@ -1,9 +1,18 @@
 const db = require('../connection');
+const { getUsers } = require('./users');
 
-const createSellListing = (sellItem) => {
+const getUserIdFromEmail = (email) => {
+  return db.query( `
+  select id from users
+  where email = $1;
+  `, [email]);
+}
+
+const createSellListing = (sellItem, owner_id) => {
   const {
     title,
     price,
+    photo,
     location_city,
     location_province,
     condition,
@@ -11,10 +20,13 @@ const createSellListing = (sellItem) => {
   } = sellItem;
 
   return db.query(`
-  INSERT INTO items (title, price, photo, location_city, location_province, category, condition, description, created_at)
-  VALUES ($1, $2, 'white_desk.jpg', $3, $4, 'desk', $5, $6, Now())
+  INSERT INTO items (title, price, photo, location_city, location_province, category, condition, description, created_at, owner_id)
+  VALUES ($1, $2, $3, $4, $5, 'desk', $6, $7, Now(), $8)
   RETURNING *;
-`, [title, price, location_city, location_province, condition, description]);
+`, [title, price, photo, location_city, location_province, condition, description, owner_id]);
 };
 
-module.exports = { createSellListing };
+module.exports = { createSellListing, getUserIdFromEmail };
+
+
+
