bc42fa274a88556a3a19ee14d194989975450fa0
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-05-11 16:48:45 -0400

    update db schema and seeds to accomodate msging system

--- db/schema/06_messages.sql
+++ db/schema/06_messages.sql
@@ -2,8 +2,7 @@ DROP TABLE IF EXISTS messages CASCADE;
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
   sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
-  messages TEXT,
+  conversation_id INTEGER REFERENCES conversations(id) ON DELETE CASCADE,
+  message TEXT,
   time_sent TIMESTAMP
 );
-
--- db/seeds/04_messages.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO messages (sender_id, item_id) VALUES (2, 4);
-
-INSERT INTO messages (sender_id, item_id) VALUES (1, 6);
-
-INSERT INTO messages (sender_id, item_id) VALUES (2, 4);
-
-INSERT INTO messages (sender_id, item_id) VALUES (3, 1);
-
-INSERT INTO messages (sender_id, item_id) VALUES (5, 4);
